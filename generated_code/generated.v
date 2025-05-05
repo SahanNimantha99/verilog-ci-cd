3be7dabd785194ef8bcf7fb3296754532d2a0db9
Sahan Nimantha <e19205@eng.pdn.ac.lk>
2025-05-05 10:10:42 +0530

    update

--- generated_code/generated.v
+++ generated_code/generated.v
@@ -1,16 +1,56 @@
-module up_counter (
-  input clk,   // clock signal
-  input load,  // load enable signal
-  input [3:0] data, // data to be loaded into the counter
-  output reg [3:0] count   // output of the counter
-);
+module TOP(CLK, RST_X, IN, OUT);
+  input CLK, RST_X;
+  input [7:0] IN;
+  output [7:0] OUT;
 
-always @(posedge clk) begin
-  if (load) begin
-    count <= data;
-  end else begin
-    count <= count + 1'b1;
+  reg [7:0] state;
+  reg [7:0] cnt;
+  
+  reg [7:0] ncnt;
+  reg [7:0] nstate;
+  reg ctrljump;
+
+  always @* begin
+
+    case(state)
+      0: begin
+        ncnt = 0;
+        ctrljump = 1;
+        if(ctrljump) begin
+          OUT = 8'h0;
+          nstate = 1;
+        end
+      end
+      
+      1: begin
+        ncnt = cnt + 1;
+        ctrljump = cnt > 7;
+        if(ctrljump) begin
+          OUT = cnt;
+          nstate = 2;
+        end
+      end
+      
+      2: begin
+        ncnt = cnt + 2;
+        ctrljump = cnt > 20;
+        if(ctrljump) begin
+          OUT = cnt - 8;
+          nstate = 0;
+        end
+      end
+    endcase
   end
-end
-
+  
+  always @(posedge CLK) begin
+    if(!RST_X) begin
+      state <= 0;
+      ncnt <= 0;
+    end else begin
+      state <= nstate;
+      cnt <= ncnt;
+    end
+  end
+  
 endmodule
+
--- validation/lint_check.py
+++ validation/lint_check.py
@@ -3,9 +3,9 @@ import subprocess
 def run_verilator(file):
     result = subprocess.run(["verilator", "--lint-only", file], capture_output=True, text=True)
     if result.returncode == 0:
-        print(f"{file}: ✅ Linting successful!")
+        print(f"{file}: Linting successful!")
     else:
-        print(f"{file}: ❌ Linting failed!\n{result.stderr}")
+        print(f"{file}: Linting failed!\n{result.stderr}")
 
 if __name__ == "__main__":
     run_verilator("generated_code/generated.v")
